b3d35c6872d080d081710ba451ead7c897fd2e32
Hamburger <72227790+bwrbur@users.noreply.github.com>
2023-09-22 22:23:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,28 +23,24 @@ const animals = [
   'squirrel',
   'fish'
 ];
-const base = 'https://api.animality.xyz';
-
-let fetch;
 
 /**
  * @typedef {Object} AnimalObject
- * @property {string} name
+ * @property {string} type
+ * @property {string} animal
  * @property {string} image
  * @property {string} fact
+ * @property {string} image_id
+ * @property {string} fact_id
  */
 
 module.exports = {
   /**
    * Returns an image and a fact of the specified animal type(s).
    * @param {string | string[]} [type='random'] The animal type(s).
-   * @param {string} [apiKey] The API key.
-   * @returns {AnimalObject | AnimalObject[]} The image and fact object.
+   * @returns {AnimalObject | AnimalObject[]} The data object.
    */
-  async getAsync(type = 'random', apiKey) {
-    if (fetch === undefined)
-      ({ default: fetch } = await import('node-fetch'));
-
+  async getAsync(type = 'random') {
     const isArray = Array.isArray(type);
     if ((typeof type !== 'string' && !isArray) || isArray && (type = type.flat()) && !type.every(t => typeof t === 'string')) throw new TypeError("'type' must be a string or an array of strings");
 
@@ -52,19 +48,15 @@ module.exports = {
     
     if (!isArray && !animals.includes(type)) throw new TypeError(`'${type}' is not a valid type, the valid types are: ${animals.join(', ')}, random`);
  
-    if (isArray) return Promise.all(type.map(t => this.getAsync(t, apiKey)));
+    if (isArray) return Promise.all(type.map(t => this.getAsync(t)));
 
-    const fetchOptions = {
-      headers: { 'X-API-Key': typeof apiKey === 'string' ? apiKey : null }
-    };
+    try {
+      const animalResponse = await fetch(`https://api.animality.xyz/all/${type}`).then(res => res.json());
+      const { animal, image, fact, image_id, fact_id } = animalResponse;
 
-    const [{ image }, { fact }] = await Promise.all([
-      fetch(`${base}/img/${type}`, fetchOptions).then(res => res.json()),
-      fetch(`${base}/fact/${type}`, fetchOptions).then(res => res.json())
-    ]).catch(err => {
+      return { type, animal, image, fact, image_id, fact_id };
+    } catch (err) {
       throw new Error(`Failed to get type '${type}' from API:\n${err}`);
-    });
-
-    return { name: type, image, fact };
+    }
   }
 };
